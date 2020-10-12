8650017b273c401f49f8baec5e47fe9e0f8a2665
jrhabima <jrhabima@uark.edu>
2020-10-12 12:40:50 -0500

    cleaned the code

--- R/mcmc_lm_mh.R
+++ R/mcmc_lm_mh.R
@@ -1,35 +1,3 @@
-# Metropolis Hastings
-library(MASS)
-library(mvtnorm)
-library(invgamma)
-#library(ggplot2)
-library(here)
-library(coda)
-library(tidyverse)
-library(tidybayes)
-library(bayesplot)
-source(here::here("R", "convert-to-coda.R"))
-
-# Gibbs sampling
-
-##create the data for a simple linear regression
-
-set.seed(111)
-k <- 5
-n <- 100
-mu_0 <- rep(5,k)
-a <- 1
-b <- 1
-sigma_sq <- rinvgamma(a,b)
-X <- cbind(1,mvrnorm(n, mu_0, diag(sqrt(sigma_sq), nrow = k, ncol = k)))
-p = ncol(X)
-mean_true_beta <- rep(0, 6)
-true_beta <- mvrnorm(1, mean_true_beta, diag(6, nrow = p, ncol = p))
-y <- X %*% true_beta + rnorm(n)
-
-plot(y, X %*% true_beta)
-
-# Metropolis Hastings algorithm
 
 mcmc_lm_mh <- function(niter, beta_tune, sigma2_tune){
     
@@ -90,66 +58,23 @@ mcmc_lm_mh <- function(niter, beta_tune, sigma2_tune){
               
         # Include the acceptance ratio
         
-        #a <- logical(length = niter)
+        a <- logical(length = niter)
+        
         
         if (runif(1) < alpha) {
-            beta_chain[i, ] <- proposed_beta
+            beta_chain[i,] <- proposed_beta
             sigma2_chain[i] <- proposed_simga2
-        } else {
-            beta_chain[i, ] <- beta_chain[i-1, ]
+            a[i] <- TRUE
+  } else {
+            beta_chain[i,] <- beta_chain[i-1,]
             sigma2_chain[i] <- sigma2_chain[i-1]
         }
-        
-  #       if (runif(1) < alpha) {
-  #           beta_chain[i,] <- proposed_beta 
-  #           sigma2_chain[i] <- proposed_simga2
-  #           a[i] <- TRUE 
-  # } else {
-  #           beta_chain[i,] <- beta_chain[i-1,] 
-  #           sigma2_chain[i] <- sigma2_chain[i-1] 
-  #       }
         
     } 
     return(list(
         beta_chain = beta_chain,
-        sigma2_chain = sigma2_chain
-        #a = a
+        sigma2_chain = sigma2_chain,
+        a = a
     ))
 }
-
-
-# mcmc_lm_mh results
-
-beta_tune <- rep(0.01, p)
-sigma2_tune <- 0.01
-niter <- 5000
-out_mh = mcmc_lm_mh(niter, beta_tune, sigma2_tune)
-
-# Trace plots
-
-out_mh$beta_chain
-post_sigma2 <- out_mh$sigma2_chain
-post_beta <- out_mh$beta_chain
-
-plot(out_mh$sigma2_chain, type = "l" )
-abline(h = true_sigma_sq)
-matplot(out_mh$beta_chain, type = "l",  xlab = "iterations", col = c("red",  "blue", "green", "purple", "black"))
-abline(h = true_beta, col = c("red",  "blue", "green", "purple", "black"))
-
-# Accepted samples
-
-acceptance <- out_mh$a
-accepted_samples <- length(acceptance[acceptance[TRUE]])
-accepted_samples
-
-accept_ratio <- 100*(accepted_samples/niter)
-accept_ratio
-
-# Or
-
-accept_ratio1 <- 100*mean(as.numeric(out_MH$a))
-accept_ratio1
-
-
-
 
