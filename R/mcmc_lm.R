87e53d4a7d8f0661ae9e6bf1cc7cc9e213bb2bcc
jrhabima <jrhabima@uark.edu>
2020-11-09 04:42:59 -0600

    modifies R files and updated the manuscript

--- R/mcmc_lm.R
+++ R/mcmc_lm.R
@@ -1,32 +1,46 @@
+library(mvnfast)
+library(ggplot2)
+library(truncnorm)
+library(coda)
+library(invgamma)
+
+set.seed(100)
+# n = 100
+# p <- 5 # length of truebeta
+# truesigma2 = 4
+# truebeta <- c(rmvn(1, rep(2, p), diag(20, nrow = p, ncol = p)))
+# X <- cbind(1, rmvn(n, rep(0, p-1), diag(1, nrow = p-1, ncol = p-1)))
+# y <- X %*% truebeta + rnorm(n, mean = 0,  sd = sqrt(truesigma2))
+# 
+# plot(y, X %*% truebeta, main = " Response vs expected response", ylab = "expecte response", cex.lab = 1, cex.main=1)
 
 mcmc_lm <- function(niter) {
     
-    set.seed(100)
     #Initial values
     
     beta <- matrix(0, niter, p)
-    sigma_sq <- rep(0, niter)
-    sigma_sq[1] <- 1
+    sigma2 <- rep(0, niter)
+    sigma2[1] <- 1
     
     # sample beta 
     
     for(i in 2 : niter){
         
         mu <- solve(t(X) %*% (X)) %*% (t(X) %*% (y)) # mean of beta
-        Sigma <- solve(t(X) %*% (X)) * sigma_sq[i-1] # covariance matrix for beta
-        beta[i,] <- mvrnorm(1, mu, Sigma)
+        Sigma <- solve(t(X) %*% (X)) * sigma2[i-1] # covariance matrix for beta
+        beta[i,] <- rmvn(1, mu, Sigma)
         
         # sample sigma_sq
         
         b_n <- 0.5 * t(y - X %*% beta[i,]) %*% (y - X %*% beta[i,]) + 1
         a_n <- (n/2) + 1
-        sigma_sq[i] <- rinvgamma(1, a, rate = b)
+        sigma2[i] <- rinvgamma(1, a_n, rate = b_n)
         
     }
     return(
         list(
             beta = beta, 
-            sigma_sq = sigma_sq
+            sigma2 = sigma2
         )
         
     )
@@ -34,3 +48,19 @@ mcmc_lm <- function(niter) {
     
     
 }
+
+# 
+# 
+# out_lm <- mcmc_lm(niter = 10000) 
+# 
+# colors <- c("blue", "red", "green", "purple", "darkblue", "black")
+# matplot(out_lm[[1]], type  = "l", col = colors)
+# abline(h = truebeta, col = colors)
+# 
+# 
+# save(out_lm,  file = here::here("results", "out_lm.RData"))
+# 
+# 
+
+
+
--- R/mcmc_lm_mh.R
+++ R/mcmc_lm_mh.R
@@ -1,80 +1,103 @@
+library(mvnfast)
+library(ggplot2)
+library(truncnorm)
+library(coda)
 
-mcmc_lm_mh <- function(niter, beta_tune, sigma2_tune){
-    
-    set.seed(222)
-    
-    # Prior distribution
-    
-    prior <- function(beta, sigma2){  
-        
-        prior_beta <- dmvnorm(beta, rep(0, p), diag(1, nrow = p, ncol = p), log = T) 
-        prior_sigma2  <- dunif(sigma2 , min = 0, max = 30, log = T)  
-        return(prior_beta + prior_sigma2)
-    }
-    
-    # Likelihood function
-    
-    likelihood <- function(beta, sigma2){   
-        
-        mean_y = X %*% beta
-        log_likelihood = dnorm(y, mean_y, sd = sqrt(sigma2), log = T)
-        return(sum(log_likelihood))   
-    }
-    
-    
-    # Posterior
-    
-    posterior <- function(beta, sigma2){
-        
-        return(prior(beta, sigma2) + likelihood(beta, sigma2))
-        
-    }
-    
-   # Initialize parameters
-    
-    beta_chain        <- matrix(0,  nrow = niter, ncol = p)
-    sigma2_chain      <- rep(0, niter)
-    beta_chain[1,]    <- mvrnorm(1, rep(0, p), diag(1, nrow = p, ncol = p)) 
-    sigma2_chain[1]   <- rinvgamma(1, shape = 1, rate =1)
+set.seed(111)
+n = 100
+p <- 5 # length of truebeta
+truesigma2 = 4
+truebeta <- c(rmvn(1, rep(2, p), diag(20, nrow = p, ncol = p)))
+X <- cbind(1, rmvn(n, rep(50, p-1), diag(1, nrow = p-1, ncol = p-1)))
+y <- X %*% truebeta + rnorm(n, mean = 0,  sd = sqrt(truesigma2))
 
-    
-    # Metropolis-Hastings step
-    
-    for (i in 2 : niter){ 
-        
-        proposed_beta <- mvrnorm(1, beta_chain[i-1,], diag(beta_tune))
-        proposed_simga2 <- rnorm(1, sigma2_chain[i-1], sigma2_tune)
-        
-        if(proposed_simga2 < 0){
-            proposed_simga2 <- sigma2_chain[i-1]
+plot(y, X %*% truebeta, main = " Response vs expected response", ylab = "expecte response", cex.lab = 1, cex.main=1)
 
-        } else {
+#mcmc_lm_mh <- function(niter, beta_tune, simga2_tune){
 
-            proposed_simga2 <- proposed_simga2
-        }
-        
-        accep_ratio  <- exp(posterior(proposed_beta, proposed_simga2) - posterior(beta_chain[i-1,], sigma2_chain[i-1]))
-              alpha  <- min(1, accep_ratio)
-              
-        # Include the acceptance ratio
-        
-        a <- logical(length = niter)
-        
-        
-        if (runif(1) < alpha) {
-            beta_chain[i,] <- proposed_beta
-            sigma2_chain[i] <- proposed_simga2
-            a[i] <- TRUE
-  } else {
-            beta_chain[i,] <- beta_chain[i-1,]
-            sigma2_chain[i] <- sigma2_chain[i-1]
-        }
-        
-    } 
-    return(list(
-        beta_chain = beta_chain,
-        sigma2_chain = sigma2_chain,
-        a = a
-    ))
+# Prior distribution
+
+prior = function(param){
+  beta   = param[1:p]
+  sigma2 = param[p + 1]
+  
+  prior_beta <-  dmvn(beta, rep(0, p), diag(10, nrow = p, ncol = p), log = T) 
+  prior_sigma2 <- dunif(sigma2, min=0, max=30, log = T)
+  return(prior_beta +  prior_sigma2)
 }
+
+# Likelihood function
+
+likelihood = function(X, y, param){
+  beta   = param[1:p]
+  sigma2 = param[p + 1]
+  
+  l = sum(dnorm(y, mean = X %*% beta, sd = sqrt(sigma2), log = T))
+  return(l)
+}
+
+# Posterior distribution
+
+posterior <- function(X, y, param){
+  return(prior(param) + likelihood(X, y,param))
+}
+
+mcmc_multi_lm <- function(X, y, K, proposal_sd){
+  chain = array(dim = c(K, p+1))
+  chain[1,] = rep(2, p+1)
+  accept <- 0
+  for (i in 2:K){
+    current_chain = chain[i-1,]
+    #proposal <- rmvn(p+2, mean = current_chain, sd = proposal_sd)
+    proposal <- as.vector(rmvn(1, current_chain, sigma = diag(proposal_sd, p+1), isChol = TRUE))
+    
+    if (proposal[p+1] < 0){
+      proposal[p+1] <- -proposal[p+1]
+    } else {
+      proposal <- proposal
+    }
+    probab = exp(posterior(X, y, proposal) - posterior(X, y, current_chain))
+    
+    if (runif(1) < probab){
+      chain[i,] = proposal
+      accept <- accept + 1 / K
+    }else{
+      chain[i,] = current_chain
+    }
+  }
+  message("acceptance ratio is", accept)
+  return(mcmc(chain))
+}
+
+
+chain1 = mcmc_multi_lm(X = X, y = y, 10000, rep(10, p+1))
+chain2 = mcmc_multi_lm(X = X, y = y, 10000, rep(1, p+1))
+chain3 = mcmc_multi_lm(X = X, y = y, 10000, rep(0.1, p+1))
+
+png(file = here::here("figures", "MH_tuning_choices.png"),  #Creating a plotting window in folder
+    res = 400, height = 6, width = 3, units = "in") # This number can be adjusted for a better view
+
+par(mfrow =c(3, 1))
+
+colors <- c("blue", "red", "green", "purple", "darkblue", "black", "violet")
+matplot(chain1, type = "l", main = "proposal sd = 10", col = colors,
+        cex.main = 1, xlab = "iterations", ylab = "parameter value")
+#abline(h = c(truebeta0,  truebeta,  truesigma2), col = colors)
+
+matplot(chain2, type = "l", main = "proposal sd = 1", col = colors,
+        cex.main = 1, xlab = "iterations", ylab = "parameter value")
+#abline(h = c(truebeta0,  truebeta,  truesigma2), col = colors)
+
+matplot(chain3, type = "l", main = "proposal sd = 0.1", col = colors,
+        cex.main = 1, xlab = "iterations", ylab = "parameter value")
+#abline(h = c(truebeta0,  truebeta,  truesigma2), col = colors)
+
+dev.off() #Save the graph to the window 
+
+# colors <- c("blue", "red", "green", "purple", "darkblue", "black")
+# chain4 = mcmc_multi_lm(X = X, y = y, 100000, c(2, rep(0.25, p+1)))
+# matplot(chain4, type = "l", main = "proposal sd = 0.1", col = colors,
+#         cex.main = 1, xlab = "iterations", ylab = "parameter value", ylim = c(-10, 10))
+# abline(h = c(truebeta,  truesigma2), col = colors)
+
 
